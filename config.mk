5aba95b7d10d38356e4bdd3b0d47d8f207432c8e
jodimcjodson <83870316+DaQueenJodi@users.noreply.github.com>
2022-10-20 21:15:33 -0500

    uwu

---
+++ config.mk
@@ -0,0 +1,14 @@
+_VERSION = 0.3.1-dev
+VERSION  = `git describe --long --tags --dirty 2>/dev/null || echo $(_VERSION)`
+
+PKG_CONFIG = pkg-config
+
+# paths
+PREFIX = /usr/local
+MANDIR = $(PREFIX)/share/man
+
+XWAYLAND =
+XLIBS =
+# Uncomment to build XWayland support
+#XWAYLAND = -DXWAYLAND
+#XLIBS = xcb
