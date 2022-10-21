70f0371fa35f5f544a5bc33d22934a752f132288
jodimcjodson <83870316+DaQueenJodi@users.noreply.github.com>
2022-10-20 21:55:30 -0500

    added placeholder to tags list to get rid of error

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const Rule rules[] = {
 	/* examples:
 	{ "Gimp",     NULL,       0,            1,           -1 },
 	*/
-	//{ "firefox",  NULL,       1 << 8,       0,           -1 },
+	{ "placeholder-uwu",  NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
