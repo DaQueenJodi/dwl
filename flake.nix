652afed5390e7d6173635d42cdc1e54385e1286e
jodimcjodson <83870316+DaQueenJodi@users.noreply.github.com>
2022-10-20 21:19:49 -0500

    fixed typo in flake

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   }: let
     system = "x86_64-linux";
   in {
-    packages.${system}.dwm = nixpkgs.legacyPackages.${system}.dwl.overrideAttrs (
+    packages.${system}.dwl = nixpkgs.legacyPackages.${system}.dwl.overrideAttrs (
       old: {
         src = builtins.path {
           name = "dwl-source";
